38a5dcf1e240cc03428ad72c86a6d50b50a95d2c
sweetpatry <giselle.donari@gmail.com>
2023-03-02 21:51:15 -0500

    ajustes

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const resp = document.querySelector("#resp")
 
 async function obtenerTodos(){
-  const response = await fetch('/.netlify/functions/lista');
+  const response = await fetch('/lista');
   const data = await response.json();  
   for (let valor of data){
     let dif=parseFloat(valor.pib_2020)-parseFloat(valor.pib_2019)
